ce28bf1ccccc73bc5d2a355833bc1aecde2e805b
BenjaminJMcD <BenjaminJMcD@outlook.com>
2023-10-18 14:06:15 -0500

    basic layout

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
 
+const myLibrary = [
+    Book("The Fellowship of the Ring", "J.R. Tolkien", 495, "yes")
+];
+
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+}
+
+const card = document.createElement("div.card")
+
+function addBook() {
+    
+}
