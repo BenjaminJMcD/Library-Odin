6993d4851bbec6c86cd54e7e5a0f8da8617db354
BenjaminJMcD <BenjaminJMcD@outlook.com>
2023-10-19 14:16:07 -0500

    Added Modal. Need Add New Book Event

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 
-const myLibrary = [
-    Book("The Fellowship of the Ring", "J.R. Tolkien", 495, "yes")
-];
+// BOOK CONSTRUCTOR
+let myLibrary = [];
 
 function Book(title, author, pages, read) {
     this.title = title
@@ -10,8 +9,43 @@ function Book(title, author, pages, read) {
     this.read = read
 }
 
+let LOTR = new Book("The Fellowship of the Ring", "J.R. Tolkien", 495, "yes")
+
+myLibrary.push(LOTR);
+
+
 const card = document.createElement("div.card")
+const gridCard = document.getElementById("gridCard")
+
+// DIALOG NEW BOOK ENTRY
+
+let inputNew = document.getElementById('inputNew');
+let openDialog = document.getElementById('openDialog');
+let submitBook = document.getElementById('submitBook');
+let closeDialog = document.getElementById('closeDialog');
+let newTitle = document.getElementById('newTitle').value;
+let newAuthor = document.getElementById('newAuthor').value;
+let newPages = document.getElementById('newPages').value;
+let newRead = document.getElementById('newRead').value;
+
+
+openDialog.addEventListener("click", () => {
+    inputNew.showModal();
+});
+
+closeDialog.addEventListener("click", () => {
+    inputNew.close();
+})
+
+submitBook.addEventListener("click", () => addBook);
 
 function addBook() {
-    
+
 }
+
+
+console.log(LOTR)
+
+
+console.log(myLibrary)
+
