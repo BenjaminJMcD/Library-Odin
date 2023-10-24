fda77dd419eeb367a60748b5f377925ab18bee28
BenjaminJMcD <BenjaminJMcD@outlook.com>
2023-10-24 15:59:47 -0500

    DOM elements + renderAssay separate. + removeBook

--- scripts.js
+++ scripts.js
@@ -5,12 +5,23 @@ let myLibrary = [];
 
 // BOOK CONSTRUCTOR
 
+let form = document.getElementById('newBookForm');
+
 function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-}
+    this.title = title;
+    this.author = author;
+    this.pages = pages + " pages";
+    this.read = read;
+    // this.toggleRead = function (read) {
+    //     if (read == true) {
+    //         this.read = "Read"
+    //     }
+    //     if (read == false) {
+    //         this.read = "Not Read"
+    //     }
+    // }
+};
+
 
 // MODAL DIALOG POPUP FOR BOOK ENTRY
 
@@ -37,32 +48,36 @@ submitBook.addEventListener("click", () => {
 
 // ADD BOOK TO ASSAY WITH MODAL DIALOG
 
-let newTitle = document.getElementById('newTitle');
-let newAuthor = document.getElementById('newAuthor');
-let newPages = document.getElementById('newPages');
-let newRead = document.getElementById('newRead');
-let form = document.getElementById('newBookForm');
+ let newTitle = document.getElementById('newTitle');
+ let newAuthor = document.getElementById('newAuthor');
+ let newPages = document.getElementById('newPages');
+ let newRead = document.getElementById('newRead');
+
 
 form.addEventListener("submit", event => {
     event.preventDefault();
 })
 
+let newBook
+
 function addBookToLibrary() {
 
-    let val1 = newTitle.value;
-    let val2 = newAuthor.value;
-    let val3 = newPages.value;
-    let val4 = "";
+    val1 = newTitle.value;
+    val2 = newAuthor.value;
+    val3 = newPages.value;
+    val4 = newRead.checked;
 
-    if (newRead.checked == true) {
+    if (val4 == true) {
         val4 = "Read"
     }
-    else {
+    else if (val4 == false) {
         val4 = "Not Read"
     }
 
     newBook = new Book(val1, val2, val3, val4);
+
     myLibrary.push(newBook);
+    
     renderNewBook();
     
     inputNew.close();
@@ -75,51 +90,63 @@ function addBookToLibrary() {
 
 let cardGrid = document.getElementById("cardGrid");
 
- function renderNewBook () {
+function createBook(item) {
+
+    let newCard = document.createElement("div");
+    newCard.classList.add("card");  
+    cardGrid.appendChild(newCard);
+    newCard.setAttribute("counter", i);
+
+    let addTitle = document.createElement("h3");
+    addTitle.classList.add("title")
+    addTitle.textContent = item.title;
+    newCard.appendChild(addTitle);
+
+    let addAuthor = document.createElement("h3");
+    addAuthor.classList.add("author");
+    addAuthor.textContent = item.author;
+    newCard.appendChild(addAuthor);
+
+    let addPages = document.createElement("h4");
+    addPages.classList.add("pages");
+    addPages.textContent = item.pages;
+    newCard.appendChild(addPages);
+
+    let addRead = document.createElement("button");
+    if (item.read == "Read") {
+        addRead.classList.add("read");
+    }
+    else {
+        addRead.classList.add("notRead");
+    }
+    addRead.textContent = item.read;
+    newCard.appendChild(addRead);
+
+    let addRemove = document.createElement("button");
+    addRemove.classList.add("remove");
+    addRemove.textContent = "Remove";
+    newCard.appendChild(addRemove);
+    addRemove.setAttribute("counter", i);
+    addRemove.onclick=removeBook;
+}
+
+function renderNewBook () {
 
     cardGrid.innerHTML = "";
 
     for (i=0; i<myLibrary.length; i++) {
 
-        let newCard = document.createElement("div");
-        newCard.classList.add("card");
-        cardGrid.appendChild(newCard);
-
-        let addTitle = document.createElement("h3");
-        addTitle.classList.add("title")
-        addTitle.textContent = myLibrary[i].title;
-        newCard.appendChild(addTitle);
-
-        let addAuthor = document.createElement("h3");
-        addAuthor.classList.add("author");
-        addAuthor.textContent = myLibrary[i].author;
-        newCard.appendChild(addAuthor);
-
-        let addPages = document.createElement("h4");
-        addPages.classList.add("pages");
-        addPages.textContent = myLibrary[i].pages;
-        newCard.appendChild(addPages);
-
-        let addRead = document.createElement("button");
-        addRead.classList.add("read");
-        addRead.textContent = myLibrary[i].read;
-        newCard.appendChild(addRead);
-        
-        let addRemove = document.createElement("button");
-        addRemove.classList.add("remove");
-        addRemove.textContent = "Remove";
-        newCard.appendChild(addRemove);
+        createBook(myLibrary[i])
     }
  }
 
- // TOGGLE READ / NOT READ
-
-
-
  // REMOVE CARD
 
+ const removeBook = (e) => {
+    const index = e.target.parentNode.getAttribute("counter");
+    
+    myLibrary.splice(index, 1);
 
+    renderNewBook();
 
-
-
-
+    }
